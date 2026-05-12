acba49eb3917aceb5fdb1ccfdcf189cc168842d0
afsarAmple <afsar.siddique@ampleserv.com>
2026-05-12 09:21:04 +0300

    fix route url

--- routes/excel.routes.js
+++ routes/excel.routes.js
@@ -35,7 +35,7 @@ router.post('/export-hierarchical', excellController.exportHierarchicalExcel);
 
 //import excell
 router.post('/import', upload.single('file'), excellController.importExcel);
-router.post('/import-HierarchicalExcel', upload.single('file'), excellController.importHierarchicalExcel);
+router.post('/import-hierarchical', upload.single('file'), excellController.importHierarchicalExcel);
 
 
 
