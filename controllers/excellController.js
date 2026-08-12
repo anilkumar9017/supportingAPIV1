dbe6d05c8ea29ff3c8e8f372c487755f7c74cd72
afsarAmple <afsar.siddique@ampleserv.com>
2026-08-12 12:42:35 +0300

    fix

--- controllers/excellController.js
+++ controllers/excellController.js
@@ -1,85 +1,7 @@
-const excelService = require('../services/excell/excell.service');
 const generateHierarchicalExcel = require('../services/excell/excell-hierarchical.service');
 
 const db = require('../config/database');
 
-async function exportExcel(req, res) {
-    try {
-        const {menuCode, mode} = req.body;
-
-        const useApi = req.useApi || false;
-        const databaseName = req.databaseName;
-
-        const workbook = await excelService.generateExcel({menuCode, mode, db, databaseName, useApi});
-        
-        res.setHeader(
-            'Content-Type',
-            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        );
-
-        res.setHeader(
-            'Content-Disposition',
-            `attachment; filename="${menuCode}-${mode}.xlsx"`
-        );
-
-        await workbook.xlsx.write(res);
-
-        res.end();
-
-    } catch (error) {
-
-        console.error(error);
-
-        res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-}
-
-/* 
-    
-*/
-async function importExcel(req, res) {
-    try {
-        const { menuCode } = req.body;
-        const useApi = req.useApi || false;
-        const userObj = req?.user;
-        const databaseName = req.databaseName;
-        const file = req.file;
-
-        if (!file) {
-            return res.status(400).json({
-                success: false,
-                message: 'Excel file is required.'
-            });
-        }
-
-        const result = await excelService.importExcel({menuCode, file, db, databaseName: databaseName, useApi: useApi, userObj});
-        if(result?.errors){
-            return res.status(200).json({
-                success: false,
-                message: 'Import faild.',
-                errors: result?.errors
-            });
-        }
-        
-        return res.status(200).json({
-            success: true,
-            message: 'Import completed.',
-            data: result,
-        });
-
-    } catch (error) {
-
-        console.error(error);
-
-        return res.status(500).json({
-            success: false,
-            message: error.message
-        });
-    }
-}
 
 async function exportHierarchicalExcel(req, res) {
     try {
@@ -160,8 +82,6 @@ async function importHierarchicalExcel(req, res) {
 
 
 module.exports = {
-    exportExcel,
-    importExcel,
     exportHierarchicalExcel,
     importHierarchicalExcel
 };
--- routes/excel.routes.js
+++ routes/excel.routes.js
@@ -30,11 +30,9 @@ router.use((req, res, next) => {
 router.use(domainMiddleware);
 
 //export template, dummy, export
-//router.post('/export', excellController.exportExcel);
 router.post('/export-hierarchical', excellController.exportHierarchicalExcel);
 
 //import excell
-//router.post('/import', upload.single('file'), excellController.importExcel);
 router.post('/import-hierarchical', upload.single('file'), excellController.importHierarchicalExcel);
 
 
